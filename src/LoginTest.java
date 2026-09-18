52c48c00c66b94d17827c9e7fceff472a821121f
ibanatimalingo <ibanatimalingo5@gmail.com>
2026-09-18 11:02:09 +0200

    Add commenting for missing tests in LoginTest
    Added TODO comments for missing test cases in LoginTest.

--- src/LoginTest.java
+++ src/LoginTest.java
@@ -12,7 +12,9 @@ public class LoginTest {
 
     @Test
     public void testCheckUserNameIncorrectFormat() {
-
+        // This only tests invalid characters, not the length/format boundary.
+        // TODO: add a case like "kylename" (long, no underscore) to actually
+        // test the boundary of the OR-condition (<=5 chars OR contains '_').
         Login login = new Login("kyle!!!!!!", "Ch&&sec@ke99!", "+27838968976", "Kyle", "Name");
         assertFalse(login.checkUserName());
     }
@@ -40,4 +42,9 @@ public class LoginTest {
         Login login = new Login("kyl_1", "Ch&&sec@ke99!", "+27838968976", "Kyle", "Name");
         assertFalse(login.loginUser("kyl_1", "wrongpassword"));
     }
+
+    // TODO: missing test for checkCellPhoneNumber()
+
+    // TODO: missing null-safety test (e.g. loginUser() called with a null
+    // username or password, to confirm it doesn't throw NullPointerException)
 }
